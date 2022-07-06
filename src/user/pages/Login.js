f6d737cdb8bc298528074f4c5b8a71d0c0d907d8
lancey1 <lanceyu619@gmail.com>
2022-07-06 19:24:14 -0400

    add forms for signup and login

--- src/user/pages/Login.js
+++ src/user/pages/Login.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 function Login() {
-    return (
-        <p>
-            Login page
-        </p>
-    )
+  return (
+    <main id="main-login">
+    <form action="/login" method="post">
+      <label for="email">Email</label>
+      <input type="email" name="email" id="email"/>
+      <label for="password">Password</label>
+      <input type="password" name="password" id="password" />
+      <button>Login</button>
+    </form>
+    </main>
+  );
 }
 
 export default Login;
--- src/user/pages/Signup.js
+++ src/user/pages/Signup.js
@@ -1,11 +1,23 @@
 import React from "react";
 
 function Signup() {
-    return (
-        <p>
-            Signup Page
-        </p>
-    )
+  return (
+    <main id="main-signup">
+      <form action="/signup" method="post">
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username" value="" />
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email" value="" />
+
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" />
+
+        <label for="confirm_password">Confirm password</label>
+        <input type="password" name="confirm_password" id="confirm_password" />
+        <button>Register</button>
+      </form>
+    </main>
+  );
 }
 
 export default Signup;
