ae18a44d9f6d0938b092d2bed27f15a55377307f
Jeyna Kim <jeyna.kimm@gmail.com>
2022-07-07 03:14:33 -0400

    edit login-page format

--- src/user/pages/Login.js
+++ src/user/pages/Login.js
@@ -8,33 +8,37 @@ import './login.css'
 
 function Login() {
   return (
-    <div>
-      <Grid container>
-        <Grid item xs={3}>
-        </Grid>
-        <Grid item xs={6}>
-          <div className="login_main">
-            <div>
-              <img src={phone_img} width="454px" />
-            </div>
-            <div>
-              <div className="login_right_component">
-                <img className="login_logo" src={meme_logo} />
-                  <div className="login_signin">
-                    <input className="login_text" type="text" placeholder="Username or Email" /> 
-                    <input className="login_text" type="password" placeholder="Password" />
-                    <button className="login_button">Login</button>
-                  </div>
-                </div>
+    <main id="main-login">
+    <form action="/login" method="post">
+      <div>
+        <Grid container>
+          <Grid item xs={3}>
+          </Grid>
+          <Grid item xs={6}>
+            <div className="main-login-page">
               <div>
+                <img src={phone_img} width="454px" alt="" />
+              </div>
+              <div>
+                <div className="login_right_component">
+                  <img className="login_logo" src={meme_logo} alt="" />
+                    <div className="login_signin">
+                      <input className="login_text" type="text" placeholder="Username or Email" /> 
+                      <input className="login_text" type="password" placeholder="Password" />
+                      <button className="login_button">Login</button>
+                    </div>
+                  </div>
+                <div>
+              </div>
             </div>
           </div>
+          </Grid>
+          <Grid item xs={3}>
+          </Grid>
+        </Grid>     
         </div>
-        </Grid>
-        <Grid item xs={3}>
-        </Grid>
-      </Grid>     
-    </div>
+    </form>
+    </main>
   );
 }
 
