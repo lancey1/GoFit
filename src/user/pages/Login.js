b1b6afb11034dcdec66bfa8237b032ba624e3a2d
Jeyna Kim <jeyna.kimm@gmail.com>
2022-07-07 02:49:01 -0400

    add-login-page layout

--- src/user/pages/Login.js
+++ src/user/pages/Login.js
@@ -1,16 +1,40 @@
 import React from "react";
+import Grid from '@material-ui/core/Grid';
+
+import phone_img from '../../images/9364675fb26a.svg';
+import meme_logo from '../../images/memestagram.png'
+
+import './login.css'
 
 function Login() {
   return (
-    <main id="main-login">
-    <form action="/login" method="post">
-      <label for="email">Email</label>
-      <input type="email" name="email" id="email"/>
-      <label for="password">Password</label>
-      <input type="password" name="password" id="password" />
-      <button>Login</button>
-    </form>
-    </main>
+    <div>
+      <Grid container>
+        <Grid item xs={3}>
+        </Grid>
+        <Grid item xs={6}>
+          <div className="login_main">
+            <div>
+              <img src={phone_img} width="454px" />
+            </div>
+            <div>
+              <div className="login_right_component">
+                <img className="login_logo" src={meme_logo} />
+                  <div className="login_signin">
+                    <input className="login_text" type="text" placeholder="Username or Email" /> 
+                    <input className="login_text" type="password" placeholder="Password" />
+                    <button className="login_button">Login</button>
+                  </div>
+                </div>
+              <div>
+            </div>
+          </div>
+        </div>
+        </Grid>
+        <Grid item xs={3}>
+        </Grid>
+      </Grid>     
+    </div>
   );
 }
 
