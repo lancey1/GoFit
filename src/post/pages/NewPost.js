8d57e3378a142e0bbd86e9118ff7e960506842ee
lancey1 <lanceyu619@gmail.com>
2022-07-06 20:11:01 -0400

    add image editor

--- src/post/pages/NewPost.js
+++ src/post/pages/NewPost.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { ImageTool } from "./editTool";
 
-const NewPost = props => {
-    return (
-        <p>
-            creating new post page
-        </p>
-    )
-}
+const NewPost = (props) => {
+  return (
+    <p>
+      <ImageTool />
+      creating new post page
+    </p>
+  );
+};
 
 export default NewPost;
---
+++ src/post/pages/editTool.js
@@ -0,0 +1,32 @@
+import 'tui-image-editor/dist/tui-image-editor.css';
+import ImageEditor from '@toast-ui/react-image-editor';
+
+const myTheme = {
+  // Theme object to extends default dark theme.
+};
+
+export const ImageTool = () => (
+  <ImageEditor
+    includeUI={{
+      loadImage: {
+        path: 'img/sampleImage.jpg',
+        name: 'SampleImage',
+      },
+      theme: myTheme,
+      menu: ['shape', 'filter'],
+      initMenu: 'filter',
+      uiSize: {
+        width: '1000px',
+        height: '700px',
+      },
+      menuBarPosition: 'bottom',
+    }}
+    cssMaxHeight={500}
+    cssMaxWidth={700}
+    selectionStyle={{
+      cornerSize: 20,
+      rotatingPointOffset: 70,
+    }}
+    usageStatistics={true}
+  />
+);
