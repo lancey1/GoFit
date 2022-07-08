7210d862d8ab8b6538a9f5c0b65b7d62859219ab
lancey1 <lanceyu619@gmail.com>
2022-07-08 01:28:25 -0400

    add create page

--- src/post/pages/NewPost.js
+++ src/post/pages/NewPost.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./NewPost.module.css";
 
-const NewPost = (props) => {
+function NewPost(props){
+  const [selectedImage, setSelectedImage] = useState();
+
+  // This function will be triggered when the file field change
+  const imageChange = (e) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setSelectedImage(e.target.files[0]);
+    }
+  };
   return (
     <main>
-      <p>creating new post page</p>
+      <h3>New Post</h3>
+      <div className={`${styles.container}`}>
+        <div className={`${styles.input}`}>
+          <input accept="image/*" type="file" onChange={imageChange} />
+        </div>
+        {selectedImage && (
+          <div>
+            <div className={`${styles.preview}`}>
+              <img
+                src={URL.createObjectURL(selectedImage)}
+                className={`${styles.image}`}
+                alt="preview"
+              />
+              <button onClick={()=>setSelectedImage()}>Remove image</button>
+            </div>
+          </div>
+        )}
+        <div className={`${styles.form}`}>
+          <form>
+            <input type="text" placeholder="Title" />
+            <input type="text" placeholder="Add a Description" />
+            <input type="text" placeholder="Add a Location" />
+            <button>Submit</button>
+          </form>
+        </div>
+      </div>
     </main>
   );
 };
-
 export default NewPost;
