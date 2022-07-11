9d47ebee028b3b36460fec5c5439aea81eddd626
Jeyna Kim <jeyna.kimm@gmail.com>
2022-07-11 17:03:53 -0400

    add collection carousel and adjust styling

--- src/collection/components/CollectionItem.js
+++ src/collection/components/CollectionItem.js
@@ -1,19 +1,51 @@
 import React from "react";
+import Carousel from "react-elastic-carousel";
 import { useHistory } from "react-router-dom";
 import styles from "./CollectionItem.module.css";
 
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  { width: 550, itemsToShow: 2 },
+  { width: 768, itemsToShow: 3 },
+  { width: 1200, itemsToShow: 3 },
+];
+
 const CollectionItem = (props) => {
+
   const history = useHistory();
 
   const clickHandler = () => {
     history.push(`/:userId/collections/${props.id}`);
   };
 
+  // const list = collectionData.map((elm) => {
+  //   return (
+  //     <div className={styles.col_item} onClick={clickHandler}>
+  //       <h1>{elm.title}</h1>
+  //       <img className={styles.col_img} src={elm.postImage} alt="" />
+  //     </div>
+  //   );
+  // });
+
+  const testList = props.data.map((elm) => {
+    return (
+      <div className={styles.col_item} onClick={clickHandler}>
+        <h1>{elm.title}</h1>
+        <img className={styles.col_img} src={elm.postImage} alt="" />
+      </div>
+    );
+  });
+
   return (
-    <div className={`${styles.col_item}`} onClick={clickHandler}>
-      <h1>{props.title}</h1>
-      <img className={`${styles.col_img}`} src={props.postImage} />
-    </div>
+    <>
+      <h1 style={{ textAlign: "center" }}>{props.title}</h1>
+      <div className={styles.col_container}>
+        <Carousel
+          breakPoints={breakPoints}>
+          {testList}
+        </Carousel>
+      </div>
+    </>
   );
 };
 
