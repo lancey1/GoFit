757882d3d17c90abdeeb9ffd870cd9e7c3c2415f
Bowen Jiang <bopepsi@jiangguangweideMacBook-Pro.local>
2022-07-13 22:44:52 -0400

    fix-style

--- src/user/components/FellowsItem.js
+++ src/user/components/FellowsItem.js
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import styles from "./FellowsItem.module.css";
 import { useHistory } from "react-router-dom";
 import FollowButton from "./FollowButton";
@@ -7,21 +7,27 @@ import { AuthContext } from '../../context/AuthContext';
 function FellowsItem(props) {
   const auth = useContext(AuthContext);
   const history = useHistory();
-  const { name, image, userId} = props;
+  const { name, image, userId } = props;
   const viewProfile = () => {
     history.push(`/user/${userId}`);
   }
 
   return (
-    <div className={`${styles.fellow_container}`}>
-      <div onClick={viewProfile}>
-        <img className={`${styles.image}`}src={image} alt="" />
-        <p className={styles.fellow_p}>{name}</p>
+    <React.Fragment>
+      <div className={`${styles.fellow_container}`}>
+
+        <div onClick={viewProfile} className={`${styles.avatar_p}`}>
+          <img className={`${styles.image}`} src={image} alt="" />
+        </div>
+
+        <div className={`${styles.action}`}>
+          <p >{name}</p>
+          {auth.isLoggedIn && <FollowButton {...props} numChanger={props.numChanger} />}
+        </div>
+
       </div>
-      <div className={styles.button_ctn}>
-       {auth.isLoggedIn && <FollowButton {...props} numChanger = {props.numChanger}/>}
-      </div>
-    </div>
+      <hr />
+    </React.Fragment>
   );
 }
 
