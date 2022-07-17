6a3220d319e8633f25c6e2dd829c3dc7708829b4
Jeyna Kim <jeyna.kimm@gmail.com>
2022-07-17 02:00:54 -0400

    fix collectionitem, collectionlist

--- src/collection/components/CollectionList.js
+++ src/collection/components/CollectionList.js
@@ -110,11 +110,11 @@ const CollectionList = (props) => {
     <div>
       {error && <ErrorModal error={error} onClear={() => setError(null)} />}
       <form onSubmit={addCollectionHandler} >
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-          <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-        </svg>
-        <input className={styles.colinput} type='text' placeholder="Add a New Collection?" value={title} onChange={inputChangeHandler} />
-        <button className={styles.colcreate_btn}>Create</button>
+        <div className={styles.collectionContainer}>
+          <img className={styles.collectionIcon} src="https://cdn-icons-png.flaticon.com/512/202/202614.png" />
+          <input className={styles.colinput} type='text' placeholder="Add a New Collection?" value={title} onChange={inputChangeHandler} />
+          <button className={styles.colcreate_btn}>Create</button>
+        </div>
       </form>
       {(collections && !isLoading) && colsList}
       {(!collections && isLoading) && <h1>Loading</h1>}
